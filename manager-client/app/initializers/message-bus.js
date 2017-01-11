5e5edf9849a5a57b671840fdbd7abd339569e0c2
Neil Lalonde <neillalonde@gmail.com>
2017-01-11 15:17:16 -0500

    fix various issues on subfolder installs

---
+++ manager-client/app/initializers/message-bus.js
@@ -0,0 +1,17 @@
+/* global MessageBus, Discourse, $:true */
+
+export default {
+  name: "message-bus",
+
+  initialize() {
+    MessageBus.baseUrl = Discourse.getURL('/');
+
+    if (MessageBus.baseUrl !== '/') {
+      MessageBus.ajax = function(opts) {
+        opts.headers = opts.headers || {};
+        opts.headers['X-Shared-Session-Key'] = $('meta[name=shared_session_key]').attr('content');
+        return $.ajax(opts);
+      };
+    }
+  }
+};
